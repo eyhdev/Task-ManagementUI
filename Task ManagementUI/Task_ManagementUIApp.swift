272b6e61e0399d22cd07d613c10c69c7f07ba8fe
eyhdev <alaqsa335@gmail.com>
2023-09-18 19:46:16 +0300

    Initial Commit

---
+++ Task ManagementUI/Task_ManagementUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementUIApp.swift
+//  Task ManagementUI
+//
+//  Created by eyh.mac on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
